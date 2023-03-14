51600441504d1458b44745e6ec5130a5ca801365
naybiblu <iamsuperv15@gmail.com>
2023-03-14 18:16:13 +0800

    Designed sign in/up page

--- src/lib/components/TopNav.svelte
+++ src/lib/components/TopNav.svelte
@@ -1,6 +1,6 @@
 <section class="w-full h-[4.5rem] flex fixed top-0 left-0 items-center px-20 justify-between">
     <a href="/" class="w-max h-max">
-        <div class="flex gap-5 items-center tracking-[0.5rem] fomt-extrabold">
+        <div class="flex gap-5 items-center tracking-[0.5rem] font-extrabold">
             <img src="img/logo.svg" alt="hisvelte" class="h-10"/>
             <p class="font-[1000]">HI-SVELTE</p>
 	    </div>  
